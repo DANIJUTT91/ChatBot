393d7d08f2f4630fb4b56ee823be855385410900
DANIJUTT91 <danijutt9119@gmail.com>
2023-09-02 01:20:57 +0500

    Added whole project

---
+++ app/MsgModerl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hp on 8/5/2023.
+//
+
+#include "MsgModerl.h"
---
+++ app/MsgModerl.h
@@ -0,0 +1,16 @@
+//
+// Created by hp on 8/5/2023.
+//
+
+#ifndef CHATBOT_MSGMODERL_H
+#define CHATBOT_MSGMODERL_H
+
+
+
+class MsgModerl {
+
+};
+
+
+
+#endif //CHATBOT_MSGMODERL_H
